8ca203a900f1a16717dfd59804b20c01b51b2d21
Leyva Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2025-07-28 15:49:22 -0300

    inicializer

---
+++ Cardapio_Api/src/main/java/br/edu/com/ufersa/Cardapio_Api/Cardapio_Api/CardapioApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.com.ufersa.Cardapio_Api.Cardapio_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioApiApplication.class, args);
+	}
+
+}
---
+++ Cardapio_Api/src/test/java/br/edu/com/ufersa/Cardapio_Api/Cardapio_Api/CardapioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.com.ufersa.Cardapio_Api.Cardapio_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
